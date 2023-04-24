70cc68565d61079d71fe20c765d75d771b685cf0
Tiago B S <tiago_barbosa-silva@hotmail.com>
2023-04-24 13:46:52 -0300

    Alterando tamanho da view

--- Telecom Tools/formPrincipalTelecom.Designer.cs
+++ Telecom Tools/formPrincipalTelecom.Designer.cs
@@ -38,38 +38,42 @@
             // 
             this.tabTelecomTools.Controls.Add(this.tbQRGen);
             this.tabTelecomTools.Controls.Add(this.tbEFGen);
-            this.tabTelecomTools.Location = new System.Drawing.Point(3, 2);
+            this.tabTelecomTools.Location = new System.Drawing.Point(2, 1);
+            this.tabTelecomTools.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabTelecomTools.Name = "tabTelecomTools";
             this.tabTelecomTools.SelectedIndex = 0;
-            this.tabTelecomTools.Size = new System.Drawing.Size(1176, 846);
+            this.tabTelecomTools.Size = new System.Drawing.Size(307, 405);
             this.tabTelecomTools.TabIndex = 0;
             // 
             // tbQRGen
             // 
-            this.tbQRGen.Location = new System.Drawing.Point(4, 34);
+            this.tbQRGen.Location = new System.Drawing.Point(4, 24);
+            this.tbQRGen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbQRGen.Name = "tbQRGen";
-            this.tbQRGen.Padding = new System.Windows.Forms.Padding(3);
-            this.tbQRGen.Size = new System.Drawing.Size(1168, 808);
+            this.tbQRGen.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbQRGen.Size = new System.Drawing.Size(815, 480);
             this.tbQRGen.TabIndex = 0;
             this.tbQRGen.Text = "QR Gen";
             this.tbQRGen.UseVisualStyleBackColor = true;
             // 
             // tbEFGen
             // 
-            this.tbEFGen.Location = new System.Drawing.Point(4, 34);
+            this.tbEFGen.Location = new System.Drawing.Point(4, 24);
+            this.tbEFGen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbEFGen.Name = "tbEFGen";
-            this.tbEFGen.Padding = new System.Windows.Forms.Padding(3);
-            this.tbEFGen.Size = new System.Drawing.Size(789, 409);
+            this.tbEFGen.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbEFGen.Size = new System.Drawing.Size(299, 377);
             this.tbEFGen.TabIndex = 1;
             this.tbEFGen.Text = "EF Gen";
             this.tbEFGen.UseVisualStyleBackColor = true;
             // 
             // formTelecomTools
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1178, 844);
+            this.ClientSize = new System.Drawing.Size(309, 405);
             this.Controls.Add(this.tabTelecomTools);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "formTelecomTools";
             this.Text = "Telecom Tools";
             this.tabTelecomTools.ResumeLayout(false);
