99f958772911e4fef87458dda4c761ee8c3ba963
Sidnei Dias <sidnei_jr11@hotmail.com>
2023-04-25 11:29:38 -0300

    Criação do Model e Controler do SUME

---
+++ Telecom Tools/Controller/Ef/SetUpMenuElementsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telecom_Tools.Model.Ef;
+
+namespace Telecom_Tools.Controller.Ef
+{
+    internal class SetUpMenuElementsController : EfController
+    {
+        private SetUpMenuElements sume;
+        private int iconQualifier;
+        private byte iconIdentifier;
+
+        public SetUpMenuElementsController()
+        {
+            sume = new SetUpMenuElements();
+        }
+
+        public void SetIconProperties(int iconQualifier, int iconIdentifier)
+        {
+            this.iconQualifier = iconQualifier;
+            this.iconIdentifier = (byte)iconIdentifier;
+        }
+
+        public override string GerarEf(string menuTitle)
+        {
+            return null;
+        }
+    }
+}
---
+++ Telecom Tools/Model/Ef/SetUpMenuElements.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telecom_Tools.Model.Ef
+{
+    internal class SetUpMenuElements : EfModel
+    {
+
+        public readonly Dictionary<string, byte[]> ICON_QUALIFIER = new()
+        {
+            { "Self-explanatory", new byte[] { 0x1E, 0x02, 0x00 } },
+            { "Not self-explanatory", new byte[] { 0x1E, 0x02, 0x01 } },
+            { "Not present", Array.Empty<byte>() }
+        };
+
+        public readonly int ICON_QUALIFIER_LENGTH = 3;
+
+        public SetUpMenuElements()
+        {
+            Path = "3F00/7F10/6F54";
+            SimpleName = "SUME";
+            Length = 30;
+        }
+    }
+}
