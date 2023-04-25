7ef1bed6a128edd8570f56d0faf7ee8a539e2c9d
Sidnei Dias <sidnei_jr11@hotmail.com>
2023-04-25 11:33:49 -0300

    Criação de classes utilitárias

---
+++ Telecom Tools/Util/ByteConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telecom_Tools.Util
+{
+    internal class ByteConverter
+    {
+        private static byte[] HexStringToByteArray(string hexString)
+        {
+            hexString = hexString.Replace(" ", "").Replace("-", "");
+            var numberChars = hexString.Length;
+            var bytes = new byte[numberChars / 2];
+            for (var i = 0; i < numberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hexString.Substring(i, 2), 16);
+            return bytes;
+        }
+        public static byte[] GetBytes(string plaintext)
+        {
+            string hexStringText = GSM7bitEncoder.Encode(plaintext);
+            byte[] encoded = HexStringToByteArray(hexStringText);
+            return encoded;
+        }
+        public static byte[] PackBlock(byte[] block)
+        {
+            long x = 0;
+            for (int i = 0; i < block.Length; i += 1) x |= ((long)block[i]) << (7 * i);
+            byte[] output = new byte[(block.Length * 7 + 7) / 8];
+            for (int i = 0; i < output.Length; i += 1) output[i] = (byte)(x >> (8 * i));
+            return output;
+        }
+        public static byte[] PackInfo(byte[] info)
+        {
+            byte[] firstBlock;
+            byte[] lastBlock;
+            if (info.Length <= 8)
+            {
+                firstBlock = PackBlock(info);
+                return firstBlock;
+            }
+            else
+            {
+                firstBlock = new byte[8];
+                Array.Copy(info, 0, firstBlock, 0, firstBlock.Length);
+                firstBlock = PackBlock(firstBlock);
+                lastBlock = new byte[info.Length - 8];
+                Array.Copy(info, 8, lastBlock, 0, lastBlock.Length);
+                lastBlock = PackBlock(lastBlock);
+                byte[] concat = new byte[firstBlock.Length + lastBlock.Length];
+                Array.Copy(firstBlock, 0, concat, 0, firstBlock.Length);
+                Array.Copy(lastBlock, 0, concat, firstBlock.Length, lastBlock.Length);
+                return concat;
+            }
+        }
+    }
+}
---
+++ Telecom Tools/Util/GSM7bitEncoder.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telecom_Tools.Util
+{
+    internal class GSM7bitEncoder
+    {
+        public static string Encode(string PlainText)
+
+        {
+
+            // ` is not a conversion, just a untranslatable letter
+
+            string strGSMTable = "";
+
+            strGSMTable += "@£$¥èéùìòÇ`Øø`Åå";
+
+            strGSMTable += "Δ_ΦΓΛΩΠΨΣΘΞ`ÆæßÉ";
+
+            strGSMTable += " !\"#¤%&'()*=,-./";
+
+            strGSMTable += "0123456789:;<=>?";
+
+            strGSMTable += "¡ABCDEFGHIJKLMNO";
+
+            strGSMTable += "PQRSTUVWXYZÄÖÑÜ`";
+
+            strGSMTable += "¿abcdefghijklmno";
+
+            strGSMTable += "pqrstuvwxyzäöñüà";
+
+            string strExtendedTable = "";
+
+            strExtendedTable += "````````````````";
+
+            strExtendedTable += "````^```````````";
+
+            strExtendedTable += "````````{}`````\\";
+
+            strExtendedTable += "````````````[~]`";
+
+            strExtendedTable += "|```````````````";
+
+            strExtendedTable += "````````````````";
+
+            strExtendedTable += "`````€``````````";
+
+            strExtendedTable += "````````````````";
+
+            string strGSMOutput = "";
+
+            foreach (char cPlainText in PlainText.ToCharArray())
+
+            {
+                int unknownCharacter = 63;
+                int intGSMTable = strGSMTable.IndexOf(cPlainText);
+
+                if (intGSMTable != -1)
+
+                {
+                    strGSMOutput += intGSMTable.ToString("X2");
+
+                    continue;
+
+                }
+
+                int intExtendedTable = strExtendedTable.IndexOf(cPlainText);
+
+                if (intExtendedTable != -1)
+
+                {
+                    strGSMOutput += (27).ToString("X2");
+                    strGSMOutput += intExtendedTable.ToString("X2");
+
+                }
+
+                if (intGSMTable == -1 && intExtendedTable == -1)
+                {
+                    strGSMOutput += unknownCharacter.ToString("X2");
+                }
+            }
+
+            return strGSMOutput;
+
+        }
+    }
+}
+}
