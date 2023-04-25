8f7692cc775850593b8fdd173c6b590d2488b48d
Sidnei Dias <sidnei_jr11@hotmail.com>
2023-04-25 11:25:17 -0300

    Criação das classes abstratas EfController e Efmodel

---
+++ Telecom Tools/Controller/Ef/EfController.cs
@@ -0,0 +1,20 @@
+﻿namespace Telecom_Tools.Controller.Ef
+{
+    internal abstract class EfController
+    {
+        public abstract string GerarEf(string input);
+
+        protected void FillByteArrayWith0xFF(byte[] byteArray)
+        {
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                byteArray[i] = 0xFF;
+            }
+        }
+
+        public static int GetIndexSearchedItem(byte[] pattern, byte[] source)
+        {
+            return Array.IndexOf(source, pattern);
+        }
+    }
+}
---
+++ Telecom Tools/Model/Ef/EfModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telecom_Tools.Model.Ef
+{
+    internal abstract class EfModel
+    {
+        public string Path { get; set; } = "";
+        public string SimpleName { get; set; } = "";
+        public int Length { get; set; }
+    }
+}
