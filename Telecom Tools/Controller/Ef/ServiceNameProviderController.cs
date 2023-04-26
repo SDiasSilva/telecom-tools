738246467759db51aca2e8b941d9057b9268b4a6
Sidnei Dias <sidnei_jr11@hotmail.com>
2023-04-26 12:49:20 -0300

    Criação do Model e Controller do SPN

---
+++ Telecom Tools/Controller/Ef/ServiceNameProviderController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telecom_Tools.Model.Ef;
+using Telecom_Tools.Util;
+
+namespace Telecom_Tools.Controller.Ef
+{
+    internal class ServiceNameProviderController : EfController
+    {
+        private readonly ServiceNameProvider spn;
+        public ServiceNameProviderController(int registeredPLMNRequired, int sPNPLMNRequired)
+        {
+            spn = new ServiceNameProvider(registeredPLMNRequired, sPNPLMNRequired);
+        }
+        public override string GerarEf(string input)
+        {
+            byte[] inputByteArray = ByteUtil.GetBytes(input);
+            byte[] spnEFContent = CreateByteArrayFilledWith0xFF(spn.Length);
+            spnEFContent[0] = (spn.RegisteredPLMNRequired, spn.ServiceProviderNamePLMNRequired) switch
+            {
+                //True and True
+                (0, 0) => 0x01,
+                //True and False
+                (0, 1) => 0x03,
+                //False and True
+                (1, 0) => 0x00,
+                //False and False
+                (1, 1) => 0x02,
+                //Not present or Not present
+                _ => 0xFF,
+            };
+            Array.Copy(inputByteArray, 0, spnEFContent, 1, inputByteArray.Length);
+            return BitConverter.ToString(spnEFContent).Replace("-", " ");
+        }
+    }
+}
---
+++ Telecom Tools/Model/Ef/ServiceNameProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telecom_Tools.Model.Ef
+{
+    internal class ServiceNameProvider:EfModel
+    {
+        public int RegisteredPLMNRequired { get; set; }
+        public int ServiceProviderNamePLMNRequired { get; set; }
+        public ServiceNameProvider(int registeredPLMNRequired, int serviceProviderNamePLMNRequired)
+        {
+            Path = "3F00/7F20/6F46";
+            SimpleName = "SPN";
+            Length = 17;
+            RegisteredPLMNRequired = registeredPLMNRequired;
+            ServiceProviderNamePLMNRequired = serviceProviderNamePLMNRequired;
+        }
+    }
+}
