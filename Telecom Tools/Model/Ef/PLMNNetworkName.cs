936fbd73ca9ff063b2d2fcd26aed79f3efa295ce
Sidnei Dias <sidnei_jr11@hotmail.com>
2023-04-26 13:36:29 -0300

    Criação do Model e Controller do PNN

---
+++ Telecom Tools/Model/Ef/PLMNNetworkName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telecom_Tools.Model.Ef
+{
+    internal class PLMNNetworkName:EfModel
+    {
+        public PLMNNetworkName()
+        {
+            Path = "3F00/7F20/6FC5";
+            SimpleName = "PNN";
+            Length = 17;
+        }
+    }
+}
--- Telecom Tools/Util/GSMEncoder.cs
+++ Telecom Tools/Util/GSMEncoder.cs
@@ -123,5 +123,38 @@ namespace Telecom_Tools.Util
             Array.Copy(input, 0, output, 4, input.Length);
             return output;
         }
+
+        public static byte[] PackInfo(byte[] info)
+        {
+            byte[] firstBlock;
+            byte[] lastBlock;
+            if (info.Length <= 8)
+            {
+                firstBlock = PackBlock(info);
+                return firstBlock;
+            }
+            else
+            {
+                firstBlock = new byte[8];
+                Array.Copy(info, 0, firstBlock, 0, firstBlock.Length);
+                firstBlock = PackBlock(firstBlock);
+                lastBlock = new byte[info.Length - 8];
+                Array.Copy(info, 8, lastBlock, 0, lastBlock.Length);
+                lastBlock = PackBlock(lastBlock);
+                byte[] concat = new byte[firstBlock.Length + lastBlock.Length];
+                Array.Copy(firstBlock, 0, concat, 0, firstBlock.Length);
+                Array.Copy(lastBlock, 0, concat, firstBlock.Length, lastBlock.Length);
+                return concat;
+            }
+        }
+
+        private static byte[] PackBlock(byte[] block)
+        {
+            long x = 0;
+            for (int i = 0; i < block.Length; i += 1) x |= ((long)block[i]) << (7 * i);
+            byte[] output = new byte[(block.Length * 7 + 7) / 8];
+            for (int i = 0; i < output.Length; i += 1) output[i] = (byte)(x >> (8 * i));
+            return output;
+        }
     }
 }
