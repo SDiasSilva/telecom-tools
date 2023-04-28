994f63be1a2213449de76b22c53a8e50e2ca1cae
Sidnei Dias <sidnei_jr11@hotmail.com>
2023-04-28 15:52:20 -0300

    Adição das classes Model e Controller de PSK e classe utilitária para fazer criptografia DES

---
+++ Telecom Tools/Controller/PSKController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telecom_Tools.Model;
+using Telecom_Tools.Util;
+
+namespace Telecom_Tools.Controller
+{
+    internal class PSKController
+    {
+        private PSK psk;
+
+        public PSKController(string iccid, string masterKey)
+        {
+            psk = new(masterKey);
+            psk.Data = ConvertICCID(iccid);
+        }
+
+        public string CalculatePSK()
+        {
+            try
+            {
+                string r1 = new TripleDES(psk.KM0).Encrypt(psk.Data);
+                string r2 = new TripleDES(psk.KM1).Encrypt(psk.Data);
+                return r1 + r2;
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Master Key or ICCID with invalid size.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Master Key or ICCID with invalid characters.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return "";
+        }
+
+        private byte[] ConvertICCID(string text)
+        {
+            string last8 = text.Substring(text.Length - 16);
+            string swapped = "";
+            for (var i = 0; i < last8.Length; i += 2)
+            {
+                swapped += Swap(last8.Substring(i, 2));
+            }
+            return ByteUtil.HexStringToByteArray(swapped);
+        }
+        private string Swap(string pair)
+        {
+            string first = pair.Substring(pair.Length - 2, 1);
+            string second = pair.Substring(pair.Length - 1);
+            return second + first;
+        }
+    }
+}
---
+++ Telecom Tools/Model/PSK.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telecom_Tools.Util;
+
+namespace Telecom_Tools.Model
+{
+    internal class PSK
+    {
+        public byte[] KM0 { get; set; } = new byte[16];
+        public byte[] KM1 { get; set; } = new byte[16];
+        public byte[] Data { get; set; } = new byte[8];
+
+        public PSK(string masterKey)
+        {
+            KM0 = ByteUtil.HexStringToByteArray(masterKey);
+            Array.Copy(KM0, 0, KM1, KM1.Length - 8, 8);
+            Array.Copy(KM0, KM0.Length - 8, KM1, 0, 8);
+        }
+    }
+}
