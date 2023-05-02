d42adb2a5e53c910f731c8405bcd6f00d6178bf1
Sidnei Dias <sidnei_jr11@hotmail.com>
2023-05-02 09:54:15 -0300

    Criação do Controller de Hash

---
+++ Telecom Tools/Controller/HashController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telecom_Tools.Util;
+using Telecom_Tools.Util.Hash;
+
+namespace Telecom_Tools.Controller
+{
+    internal class HashController
+    {
+        private readonly Dictionary<int, HashType> HASH_TYPE = new()
+        {
+            { 0, new SHA1Hash()}
+        };
+
+        public HashType SelectHashAlgorithm(int hashtype)
+        {
+            return HASH_TYPE[hashtype];
+        }
+
+    }
+}
