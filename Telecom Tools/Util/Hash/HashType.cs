71b6dbc4f0f429876a859c81e927b2b1b328c345
Sidnei Dias <sidnei_jr11@hotmail.com>
2023-05-02 09:53:42 -0300

    Criado a classe SHA1Hash e uma interface equivalente

---
+++ Telecom Tools/Util/Hash/HashType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telecom_Tools.Util.Hash
+{
+    internal interface HashType
+    {
+        public string ComputeHash(string plaintext);
+    }
+}
