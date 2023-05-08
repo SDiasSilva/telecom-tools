e905ef1e3313864260d8bb52c03c7a7be7555793
Tiago B S <tiago_barbosa-silva@hotmail.com>
2023-05-08 11:23:35 -0300

    Corrigindo warns de simplificação de código do Visual Studio

--- Telecom Tools/giesecke-devrient/qr-code-generator/Controller/VersionController.cs
+++ Telecom Tools/giesecke-devrient/qr-code-generator/Controller/VersionController.cs
@@ -7,17 +7,19 @@ namespace Telecom_Tools.Controller
     {
         public void CriaSplitButtons(ComboBox comboBox)
         {
-            StringCollection strings = new StringCollection();
-            strings.Add("Auto - 0");
+            StringCollection strings = new()
+            {
+                "Auto - 0"
+            };
             Model.Version.SetVersionNumber(-1);
             for (int i = 0; i < 40; i++)
             {
-                StringBuilder sb = new StringBuilder();
-                sb.Append(21 + (i * 4)).Append("x").Append(21 + (i * 4)).Append(" -").Append(i + 1);
+                StringBuilder sb = new();
+                sb.Append(21 + (i * 4)).Append('x').Append(21 + (i * 4)).Append(" -").Append(i + 1);
                 string novaString = sb.ToString();
 
-                comboBox.SelectedIndexChanged -= comboBox_SelectedIndexChanged;
-                comboBox.SelectedIndexChanged += comboBox_SelectedIndexChanged;
+                comboBox.SelectedIndexChanged -= ComboBox_SelectedIndexChanged;
+                comboBox.SelectedIndexChanged += ComboBox_SelectedIndexChanged;
                 strings.Add(novaString);
             }
 
@@ -33,7 +35,7 @@ namespace Telecom_Tools.Controller
         }
 
 
-        private void comboBox_SelectedIndexChanged(object? sender, EventArgs e)
+        private void ComboBox_SelectedIndexChanged(object? sender, EventArgs e)
         {
             int selectedIndex = ((ComboBox)sender!).SelectedIndex;
 
