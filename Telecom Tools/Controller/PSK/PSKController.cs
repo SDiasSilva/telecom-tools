67c6aaa26962e51bd01573a23500365391f5b47a
Sidnei Dias <sidnei_jr11@hotmail.com>
2023-05-29 09:57:35 -0300

    Documentação da ferramenta de PSK

--- Telecom Tools/Controller/PSK/PSKController.cs
+++ Telecom Tools/Controller/PSK/PSKController.cs
@@ -9,17 +9,43 @@ using Telecom_Tools.Util;
 
 namespace Telecom_Tools.Controller
 {
+    /// <summary>
+    ///  This class is used to calculate the psk.
+    /// </summary>
     internal class PSKController
     {
         private readonly string iccid;
         private readonly string masterKey;
 
+        /// <summary>
+        /// This constructor will initialize the fields "iccid" and "masterKey" that will be used
+        /// to calculate the PSK.
+        /// </summary>
+        /// <param name="iccid">This parameter will be defined by the text of ICCIDTextBox.</param>
+        /// <param name="masterKey">This parameter will be defined by the text of masterKeyTextBox.</param>
         public PSKController(string iccid, string masterKey)
         {
             this.masterKey = masterKey;
             this.iccid = iccid;
         }
 
+        /// <summary>
+        /// This method calculates the psk in the following way:
+        /// <para>■ The data(data) is the 8 last bytes of the swapped ICCID</para>
+        /// <para>■ The Master Key(KM0) is a 16-byte key.</para>
+        /// <para>The algorithm for calculating diversified keys is:</para>
+        /// <para>KM0 = K1 || K2</para>
+        /// <para>KM1 = K2 || K1</para>
+        /// <para>R1 = 3 - DESKM0(data) or R1 = DESK1(DES - 1K2(DESK1(data)))</para>
+        /// <para>R2 = 3 - DESKM1(data) or R2 = DESK2(DES - 1K1(DESK2(data)))</para>
+        /// <para>PSK = R1 || R2</para>
+        /// <para>where:</para>
+        /// <para>■ K1 = Left - most 8 bytes of the Master Key</para>
+        /// <para>■ K2 = Right - most 8 bytes of the Master Key</para>
+        /// <para>The PSK is encrypted using 3DES ECB NO_PADDING and using two different 8 byte keys.</para>
+        /// <para>The PSK is 16 bytes in length</para>
+        /// </summary>
+        /// <returns>Returns the calculated PSK.</returns>
         public string CalculatePSK()
         {
             try
@@ -41,6 +67,11 @@ namespace Telecom_Tools.Controller
             return "";
         }
 
+        /// <summary>
+        /// This method converts the ICCID by swapping every 2 digits.
+        /// </summary>
+        /// <param name="iccid">This parameter will be returned swapped.</param>
+        /// <returns>Returns the swapped ICCID e.g 89256776274942161783 -> 98527667729424617138.</returns>
         private static byte[] ConvertICCID(string iccid)
         {
             string last8 = iccid[^16..];
@@ -51,8 +82,17 @@ namespace Telecom_Tools.Controller
             }
             return ByteUtil.HexStringToByteArray(swapped);
         }
+
+        /// <summary>
+        /// This method swaps 2 characters of a string with length of 2.
+        /// </summary>
+        /// <param name="pair">This parameter it is a string of 2 characters.</param>
+        /// <returns>Returns the swapped characters e.g 89 -> 98.</returns>
         private static string Swap(string pair)
         {
+            if (pair.Length > 2){
+                throw new ArgumentException("Invalid pair size");
+            }
             string first = pair.Substring(pair.Length - 2, 1);
             string second = pair[^1..];
             return second + first;
--- Telecom Tools/Model/PSK/PSK.cs
+++ Telecom Tools/Model/PSK/PSK.cs
@@ -7,12 +7,20 @@ using Telecom_Tools.Util;
 
 namespace Telecom_Tools.Model
 {
+    /// <summary>
+    ///  This class stores the neccessary attributes to calculate the PSK/TLS Key.
+    /// </summary>
     internal class PSK
     {
         public byte[] KM0 { get; set; } = new byte[16];
         public byte[] KM1 { get; set; } = new byte[16];
         public byte[] Data { get; set; } = new byte[8];
 
+        /// <summary>
+        /// This constructor will initialize the fields "KM0" and "KM1" spliting the parameter 
+        /// "masterkey".
+        /// </summary>
+        /// <param name="masterKey">This parameter will be defined by the text of masterKeyTextBox.</param>
         public PSK(string masterKey)
         {
             KM0 = ByteUtil.HexStringToByteArray(masterKey);
